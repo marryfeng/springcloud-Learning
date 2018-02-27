e813c3ab8d4b8b1acb4207ee986885aaf1eeaa99
imooc <1473357859@qq.com>
2018-02-27 21:27:18 +0800

    调用服务

---
+++ consumerservice/src/main/java/com/springcloud/learning/Service/HelloService.java
@@ -0,0 +1,19 @@
+package com.springcloud.learning.Service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+/**
+ * Created by fengli on 2018/2/27.
+ */
+@Service
+public class HelloService {
+    @Autowired
+     RestTemplate restTemplate;
+    public String hiService(String name){
+       return restTemplate.getForObject("http://PROVIDERSERVICE/provider/hi?name="+name,String.class);
+    }
+
+
+}
