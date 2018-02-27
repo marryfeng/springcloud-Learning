c12a241b1b0f980fad86509b0353c5a5282a3ee1
imooc <1473357859@qq.com>
2018-02-27 21:26:34 +0800

    测试ribbon

---
+++ consumerservice/src/main/java/com/springcloud/learning/controller/HelloController.java
@@ -0,0 +1,24 @@
+package com.springcloud.learning.controller;
+
+import com.springcloud.learning.Service.HelloService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by fengli on 2018/2/27.
+ */
+@RestController
+public class HelloController {
+
+    @Autowired
+    private HelloService helloService;
+
+    @RequestMapping("/hello")
+    public String hello(@RequestParam String name){
+       return  helloService.hiService(name);
+    }
+}
+
