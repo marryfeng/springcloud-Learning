590570a1b65799e239290e1ff0aef2c241620a2f
imooc <1473357859@qq.com>
2018-03-09 10:15:25 +0800

    zuul

---
+++ microservice-zuul/src/main/java/com/springcloud/learning/MicroserviceZuulApplication.java
@@ -0,0 +1,19 @@
+package com.springcloud.learning;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.cloud.netflix.zuul.filters.discovery.PatternServiceRouteMapper;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+@EnableZuulProxy
+public class MicroserviceZuulApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(MicroserviceZuulApplication.class, args);
+	}
+
+}
