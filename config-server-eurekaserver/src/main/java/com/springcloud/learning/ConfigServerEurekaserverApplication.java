8ccbddc3d2c02b1c3b52f7e720095f66ae854e67
imooc <1473357859@qq.com>
2018-03-13 13:51:41 +0800

    配置文件服务器

---
+++ config-server-eurekaserver/src/main/java/com/springcloud/learning/ConfigServerEurekaserverApplication.java
@@ -0,0 +1,21 @@
+package com.springcloud.learning;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+
+
+@SpringBootApplication
+@EnableEurekaClient
+@EnableConfigServer
+public class ConfigServerEurekaserverApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(ConfigServerEurekaserverApplication.class, args);
+	}
+}
