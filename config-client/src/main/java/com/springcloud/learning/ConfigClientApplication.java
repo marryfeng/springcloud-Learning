1a61cd5884e7dd36845461ce3a41bef25dc531ab
imooc <1473357859@qq.com>
2018-03-15 10:42:21 +0800

    配置的客户端

--- config-client/src/main/java/com/springcloud/learning/ConfigClientApplication.java
+++ config-client/src/main/java/com/springcloud/learning/ConfigClientApplication.java
@@ -2,8 +2,12 @@ package com.springcloud.learning;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 
 @SpringBootApplication
+@RefreshScope
+@EnableDiscoveryClient
 public class ConfigClientApplication {
 
 	public static void main(String[] args) {
